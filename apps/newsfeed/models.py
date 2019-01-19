e281e1d7bab826a3da72633e2044f546187b61b6
mattkiefer <matthewlkiefer@gmail.com>
2019-01-18 18:00:14 -0600

    str rep fxn for newsfeed items

--- apps/newsfeed/models.py
+++ apps/newsfeed/models.py
@@ -6,7 +6,8 @@ class FeedItem(models.Model):
     hed=models.CharField(max_length=280)
     link=models.CharField(max_length=1000)
     races=models.ManyToManyField(Race)
-
+    def __str__(self):
+        return self.hed
 
 """
 class RaceTag(models.Model):
