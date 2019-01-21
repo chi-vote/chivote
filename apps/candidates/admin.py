c7fe47c0f71d7317a45469f78117b85b5efca307
Patrick Judge <pjudge@bettergov.org>
2019-01-21 11:26:15 -0600

    pretty up candidate admin form

--- apps/candidates/admin.py
+++ apps/candidates/admin.py
@@ -1,22 +1,54 @@
 from django.contrib import admin
+from django.forms import ModelForm
+
+from django.utils.translation import ugettext_lazy as _
+
 from apps.candidates.models import Candidate, CandidateContact
 from apps.questionnaires.models import Response
 
 
 class ContactInline(admin.TabularInline):
     model = CandidateContact
+    extra = 0
+    min_num = 1
 
 
 class ResponseInline(admin.StackedInline):
     model = Response
+    extra = 0
 
 
+class CandidateForm(ModelForm):
+    class Meta:
+        model = Candidate
+        fields = '__all__'
+        labels = {
+            'cboe_id': _('Chicago Board of Elections'),
+            'isbe_id': _('Illinois State Board of Elections'),
+            'br_id': _('BallotReady'),
+            'ri_id': _('Reform Illinois'),
+        }
+
+
+@admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
     model = Candidate
+    form = CandidateForm
     inlines = [
         ContactInline,
         ResponseInline
     ]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'race', 'incumbent', 'ballot_order')
+        }),
+        ('Alternate keys', {
+            'classes': ('collapse',),
+            'fields': ('cboe_id', 'isbe_id', 'br_id', 'ri_id')
+        })
+    )
 
-
-admin.site.register(Candidate, CandidateAdmin)
+    # list screen
+    list_display = ('__str__', 'race', 'incumbent')
+    list_filter = ('race',)
+    ordering = ('race', 'ballot_order',)
--- apps/offices/models.py
+++ apps/offices/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from apps.places.models import Place
 
 # Create your models here.
+
+
 class Office(models.Model):
     # someday these could be subclassed if necessary
     # but that's potentially complicated to implement
     name = models.CharField(max_length=20)
-    place = models.ForeignKey(Place,on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
-    ### TODO sort out incumbency stuff later
+    # TODO sort out incumbency stuff later
     """ 
     lame_duck = models.CharField(max_length=50,null=True) # an incumbent who isn't running
     incumbent_candidate = models.ForeignKey(Candidate,null=True,on_delete=models.CASCADE) 
     def incumbent(self):
         return self.incumbent if self.incumbent else self.lame_duck
     """
- 
