d6b158c7bdf4f736c1882dd1bf0755a040c98c90
mattkiefer <matthewlkiefer@gmail.com>
2019-01-22 12:31:02 -0600

    models candidate statements and exposes to admin

--- apps/newsfeed/admin.py
+++ apps/newsfeed/admin.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-from apps.newsfeed.models import FeedItem
+from apps.newsfeed.models import FeedItem, CandidateStatement
+from apps.candidates.models import Candidate
 from apps.races.models import Race
 
 class FeedItemAdmin(admin.ModelAdmin):
-     model= FeedItem
-     filter_horizontal = ('races',) #If you don't specify this, you will get a multiple select widget.
+    model = FeedItem
+    filter_horizontal = ('races',) #If you don't specify this, you will get a multiple select widget.
+
+class StatementAdmin(admin.ModelAdmin):
+    model = CandidateStatement
 
 admin.site.register(FeedItem, FeedItemAdmin)
-
-
-"""
-from apps.newsfeed.models import FeedItem, RaceTag
-# Register your models here.
-
-admin.site.register(FeedItem)
-"""
+admin.site.register(CandidateStatement, StatementAdmin)
--- apps/newsfeed/models.py
+++ apps/newsfeed/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.races.models import Race
+from apps.candidates.models import Candidate
 
 # Create your models here.
 class FeedItem(models.Model):
@@ -10,3 +11,10 @@ class FeedItem(models.Model):
     def __str__(self):
         return self.hed
 
+class CandidateStatement(models.Model):
+    candidate = models.ForeignKey(Candidate,on_delete=models.CASCADE)
+    statement = models.TextField()
+    issue = models.CharField(max_length=100) # this should be fk once issues are modeled
+    date = models.DateTimeField()
+    def __str__(self):
+        return self.candidate.name + ' on ' + self.issue # also need to fix this when issues normalize
