ab5cb947b2800016e99674b9c56fb4c8e2847fa2
Patrick Judge <pjudge@bettergov.org>
2019-01-24 09:23:52 -0600

    add issue model to newsfeed

--- apps/newsfeed/admin.py
+++ apps/newsfeed/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from apps.newsfeed.models import FeedItem, CandidateStatement
-from apps.candidates.models import Candidate
-from apps.races.models import Race
+from apps.newsfeed.models import Article, CandidateStatement, Issue
+# from apps.candidates.models import Candidate
+# from apps.races.models import Race
 
-class FeedItemAdmin(admin.ModelAdmin):
-    model = FeedItem
-    filter_horizontal = ('races',) #If you don't specify this, you will get a multiple select widget.
 
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    model = Issue
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    # If you don't specify this, you will get a multiple select widget.
+    filter_horizontal = ('race',)
+
+
+@admin.register(CandidateStatement)
 class StatementAdmin(admin.ModelAdmin):
     model = CandidateStatement
-
-admin.site.register(FeedItem, FeedItemAdmin)
-admin.site.register(CandidateStatement, StatementAdmin)
--- apps/newsfeed/models.py
+++ apps/newsfeed/models.py
@@ -2,19 +2,35 @@ from django.db import models
 from apps.races.models import Race
 from apps.candidates.models import Candidate
 
-# Create your models here.
-class FeedItem(models.Model):
-    hed=models.CharField(max_length=280)
-    link=models.CharField(max_length=1000)
-    source=models.CharField(max_length=200)
-    races=models.ManyToManyField(Race)
+
+class Issue(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    hed = models.CharField(max_length=280)
+    date = models.DateTimeField()
+    link = models.URLField()
+    source = models.CharField(max_length=200)
+    race = models.ManyToManyField(Race)
+    issue = models.ManyToManyField(Issue)
+
     def __str__(self):
         return self.hed
 
+
 class CandidateStatement(models.Model):
-    candidate = models.ForeignKey(Candidate,on_delete=models.CASCADE)
     statement = models.TextField()
-    issue = models.CharField(max_length=100) # this should be fk once issues are modeled
     date = models.DateTimeField()
+    link = models.URLField()
+    source = models.CharField(max_length=200)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    issue = models.ManyToManyField(Issue)
+
     def __str__(self):
-        return self.candidate.name + ' on ' + self.issue # also need to fix this when issues normalize
+        # also need to fix this when issues normalize
+        return self.candidate.name + ' on ' + self.issue
