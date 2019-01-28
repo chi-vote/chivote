7c76197377749348cc2b5b48a21fa8c515b2fbae
Patrick Judge <pjudge@bettergov.org>
2019-01-28 06:58:30 -0600

    add public path to patch broken img references

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -4,9 +4,10 @@ const baseConfig = require('./webpack.base');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(baseConfig, {
-	mode: 'production',
-	output: {
-		path: path.resolve(__dirname, 'app/static/dist')
-	},
-	plugins: [new CleanWebpackPlugin(['app/static/dist'])]
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'app/static/dist'),
+    publicPath: '/static/dist/'
+  },
+  plugins: [new CleanWebpackPlugin(['app/static/dist'])]
 });
