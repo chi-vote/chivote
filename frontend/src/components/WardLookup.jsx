4d7edfe005e5510672bba74ebbf89ede08a3fc64
Patrick Judge <pjudge@bettergov.org>
2019-01-29 00:53:50 -0600

    add fuse fuzzy match

--- frontend/src/components/WardLookup.jsx
+++ frontend/src/components/WardLookup.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import Autocomplete from 'react-autocomplete';
+import Fuse from 'fuse.js';
+import _ from 'lodash';
 import 'whatwg-fetch';
+
+var fuseOptions = {
+  shouldSort: true,
+  // includeScore: true,
+  threshold: 0.3,
+  location: 0,
+  distance: 100,
+  minMatchCharLength: 1,
+  keys: ['address']
+};
 
 export default class WardLookup extends Component {
   state = {
@@ -8,7 +20,8 @@ export default class WardLookup extends Component {
     streetAddr: '',
     streetNames: [],
     addresses: [],
-    wardFound: null
+    wardFound: null,
+    fuse: new Fuse([], fuseOptions)
   };
 
   getData = () => {
@@ -17,24 +30,25 @@ export default class WardLookup extends Component {
     })
       .then(res => res.json())
       .then(addresses => {
-        this.setState({ addresses });
+        var addressObjects = Object.entries(addresses).map(x => ({
+          address: x[0],
+          ...x[1]
+        }));
+        this.setState({ addresses: addressObjects });
       });
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.streetNo !== prevState.streetNo) {
-      // console.log(
-      //   `prev streetNo: ${prevState.streetNo}; curr: ${this.state.streetNo}`
-      // );
       this.getData();
     }
-  }
 
-  handleInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+    if (this.state.addresses !== prevState.addresses) {
+      var list = this.state.addresses;
+      var fuse = new Fuse(list, fuseOptions);
+      this.setState({ fuse });
+    }
+  }
 
   handleACInput = (e, value) => {
     var streetNoStr = value.trim().split(' ')[0];
@@ -43,12 +57,6 @@ export default class WardLookup extends Component {
     this.setState({
       streetNo,
       streetAddr: value
-    });
-  };
-
-  displayWard = val => {
-    this.setState({
-      wardFound: this.state.addresses[e.target.value].ward
     });
   };
 
@@ -61,8 +69,22 @@ export default class WardLookup extends Component {
             <div className="field">
               <div className="control is-expanded">
                 <Autocomplete
-                  name="streetAddr"
-                  getItemValue={item => item}
+                  getItemValue={item => item.address}
+                  items={this.state.fuse.search(this.state.streetAddr)}
+                  renderItem={(item, isHighlighted) => (
+                    <div
+                      style={{
+                        background: isHighlighted ? 'lightgray' : 'white'
+                      }}
+                    >
+                      {item.address}
+                    </div>
+                  )}
+                  // renderItem={item => (
+                  //   <li className="list-item address-choice is-lsb is-lightblue-text">
+                  //     {item.address}
+                  //   </li>
+                  // )}
                   renderInput={props => (
                     <input
                       className="input is-rounded is-lsb"
@@ -71,21 +93,15 @@ export default class WardLookup extends Component {
                       tabIndex="2"
                     />
                   )}
-                  renderItem={item => (
-                    <li className="list-item address-choice is-lsb is-lightblue-text">
-                      {item}
-                    </li>
-                  )}
-                  shouldItemRender={(addr, value) =>
-                    addr.toLowerCase().includes(value.toLowerCase())
-                  }
                   onSelect={val =>
                     this.setState({
                       streetAddr: val,
-                      wardFound: this.state.addresses[val].ward
+                      wardFound: _.find(
+                        this.state.addresses,
+                        x => x.address === val
+                      ).ward
                     })
                   }
-                  items={Object.keys(this.state.addresses)}
                   value={this.state.streetAddr}
                   onChange={this.handleACInput}
                 />
