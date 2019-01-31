348280301965311d1f61866bb29695eefb4040c7
Patrick Judge <pjudge@bettergov.org>
2019-01-31 11:42:32 -0600

    initial celery setup

--- chivote/settings/base.py
+++ chivote/settings/base.py
@@ -203,3 +203,15 @@ BALLOT_READY_API_KEY = config('BALLOT_READY_API_KEY')
 
 # colorfield settings
 INSTALLED_APPS += ('colorfield', )
+
+# celery settings
+# Celery application definition
+# http://docs.celeryproject.org/en/v4.0.2/userguide/configuration.html
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_BEAT_SCHEDULE = {}
