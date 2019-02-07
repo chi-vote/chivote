d1318118f3e8f5f9d79d4a1b1da05af78dc0ca88
Patrick Judge <pjudge@bettergov.org>
2019-02-07 16:46:40 -0600

    pull bulma into frontend bundle

---
+++ frontend/src/loadIcons.js
@@ -0,0 +1,4 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import {} from '@fortawesome/free-solid-svg-icons';
+import {} from '@fortawesome/free-brands-svg-icons';
+import {} from '@fortawesome/free-regular-svg-icons';
