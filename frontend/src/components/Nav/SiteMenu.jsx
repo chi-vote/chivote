992d75ebbb6d86a8aaa16809ecb6f6235c7450c2
Patrick Judge <pjudge@bettergov.org>
2019-02-11 15:40:03 -0600

    support spanish links in nav and on homepage

--- frontend/src/components/Nav/SiteMenu.jsx
+++ frontend/src/components/Nav/SiteMenu.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
-export const SiteMenu = props => (
-  <Menu {...props}>
-    <a className="menu-item" href="/">
-      <i className="fas fa-home" />
-      Home
-    </a>
-    <a className="menu-item" href="/races/">
-      <i className="fas fa-vote-yea" />
-      Races
-    </a>
-    <a className="menu-item" href="/faq/">
-      <i className="far fa-question-circle" />
-      FAQ
-    </a>
-    <a className="menu-item" href="/ask/">
-      <i className="far fa-comment-alt" />
-      Ask
-    </a>
-    <a className="menu-item" href="/quiz/">
-      <i className="fas fa-gamepad" />
-      Quiz
-    </a>
-    <a className="menu-item" href="/events/">
-      <i className="fas fa-calendar" />
-      Events
-    </a>
-    <hr />
-    <div>
-      <p className="has-text-weight-bold">Contact us</p>
-      <p>Got questions? Need help? Did we miss something?</p>
-      <p>
-        Email us at <a href="mailto:info@chi.vote">info@chi.vote</a>.
-      </p>
-    </div>
-  </Menu>
-);
+export const SiteMenu = props => {
+  const currPath = window.location.pathname;
+  const urlRoot = currPath.includes('/es/') ? '/es/' : '/';
+
+  return (
+    <Menu {...props}>
+      <a className='menu-item' href={urlRoot}>
+        <i className='fas fa-home' />
+        Home
+      </a>
+      <a className='menu-item' href={urlRoot + 'races/'}>
+        <i className='fas fa-vote-yea' />
+        Races
+      </a>
+      <a className='menu-item' href={urlRoot + 'faq/'}>
+        <i className='far fa-question-circle' />
+        FAQ
+      </a>
+      <a className='menu-item' href={urlRoot + 'ask/'}>
+        <i className='far fa-comment-alt' />
+        Ask
+      </a>
+      <a className='menu-item' href={urlRoot + 'quiz/'}>
+        <i className='fas fa-gamepad' />
+        Quiz
+      </a>
+      <a className='menu-item' href={urlRoot + 'events/'}>
+        <i className='fas fa-calendar' />
+        Events
+      </a>
+      <hr />
+      <div>
+        <p className='has-text-weight-bold'>Contact us</p>
+        <p>Got questions? Need help? Did we miss something?</p>
+        <p>
+          Email us at <a href='mailto:info@chi.vote'>info@chi.vote</a>.
+        </p>
+      </div>
+    </Menu>
+  );
+};
--- frontend/src/pages/Homepage/LandingPage.jsx
+++ frontend/src/pages/Homepage/LandingPage.jsx
@@ -4,23 +4,32 @@ import researchIcon from 'Assets/images/chivote-research-icon.png';
 import readyIcon from 'Assets/images/chivote-ready-icon.png';
 
 const LandingPage = props => {
+  const currPath = window.location.pathname;
+  const urlRoot = currPath.includes('/es/') ? '/es/' : '/';
+
   return (
-    <div className="columns is-gapless">
-      <div className="column is-4">
-        <div className="voter-stage stage-questions has-text-centered">
-          <img src={questionsIcon} alt="" className="voter-stage__image" />
-          <p className="voter-stage__cta is-lsb is-size-3 is-red-text">
+    <div className='columns is-gapless'>
+      <div className='column is-4'>
+        <div className='voter-stage stage-questions has-text-centered'>
+          <img src={questionsIcon} alt='' className='voter-stage__image' />
+          <p className='voter-stage__cta is-lsb is-size-3 is-red-text'>
             Where do I start?
           </p>
-          <div className="voter-stage__links">
+          <div className='voter-stage__links'>
             <ul>
               <li>
-                <a href="/faq/" className="button is-large is-rounded">
+                <a
+                  href={urlRoot + 'faq/'}
+                  className='button is-large is-rounded'
+                >
                   Read through the FAQ
                 </a>
               </li>
               <li>
-                <a href="/quiz/" className="button is-large is-rounded">
+                <a
+                  href={urlRoot + 'quiz/'}
+                  className='button is-large is-rounded'
+                >
                   Take the quiz
                 </a>
               </li>
@@ -28,21 +37,27 @@ const LandingPage = props => {
           </div>
         </div>
       </div>
-      <div className="column is-4">
-        <div className="voter-stage stage-research has-text-centered">
-          <img src={researchIcon} alt="" className="voter-stage__image" />
-          <p className="voter-stage__cta is-lightblue-text is-lsb is-size-3">
+      <div className='column is-4'>
+        <div className='voter-stage stage-research has-text-centered'>
+          <img src={researchIcon} alt='' className='voter-stage__image' />
+          <p className='voter-stage__cta is-lightblue-text is-lsb is-size-3'>
             I'm ready to learn.
           </p>
-          <div className="voter-stage__links">
+          <div className='voter-stage__links'>
             <ul>
               <li>
-                <a href="/races/" className="button is-large is-rounded">
+                <a
+                  href={urlRoot + 'races/'}
+                  className='button is-large is-rounded'
+                >
                   Explore the races
                 </a>
               </li>
               <li>
-                <a href="/ask/" className="button is-large is-rounded">
+                <a
+                  href={urlRoot + 'ask/'}
+                  className='button is-large is-rounded'
+                >
                   Ask us anything
                 </a>
               </li>
@@ -50,26 +65,34 @@ const LandingPage = props => {
           </div>
         </div>
       </div>
-      <div className="column is-4">
-        <div className="voter-stage stage-ready has-text-centered">
-          <img src={readyIcon} alt="" className="voter-stage__image" />
-          <p className="voter-stage__cta is-lsb is-size-3">
+      <div className='column is-4'>
+        <div className='voter-stage stage-ready has-text-centered'>
+          <img src={readyIcon} alt='' className='voter-stage__image' />
+          <p className='voter-stage__cta is-lsb is-size-3'>
             I'm ready to vote!
           </p>
-          <div className="voter-stage__links">
+          <div className='voter-stage__links'>
             <ul>
               <li>
                 <a
-                  href="https://chicagoelections.com/en/register-to-vote-change-of-address.html"
-                  className="button is-large is-rounded"
+                  href={
+                    currPath.includes('/es/')
+                      ? 'https://chicagoelections.com/sp/register-to-vote-change-of-address.html'
+                      : 'https://chicagoelections.com/en/register-to-vote-change-of-address.html'
+                  }
+                  className='button is-large is-rounded'
                 >
                   Get registered
                 </a>
               </li>
               <li>
                 <a
-                  href="https://chicagoelections.com/en/your-voter-information.html"
-                  className="button is-large is-rounded"
+                  href={
+                    currPath.includes('/es/')
+                      ? 'https://chicagoelections.com/sp/your-voter-information.html'
+                      : 'https://chicagoelections.com/en/your-voter-information.html'
+                  }
+                  className='button is-large is-rounded'
                 >
                   Find your polling place
                 </a>
