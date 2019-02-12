f68a6693de70c0da84a24806e7c5b2dbb44a9665
Patrick Judge <pjudge@bettergov.org>
2019-02-12 11:19:21 -0600

    remove CleanWebpackPlugin

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,13 +1,11 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const baseConfig = require('./webpack.base');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'static/dist'),
     publicPath: '/static/dist/'
-  },
-  plugins: [new CleanWebpackPlugin(['static/dist'])]
+  }
 });
