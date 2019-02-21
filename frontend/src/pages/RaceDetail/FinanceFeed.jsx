e8fa1ebef1cb80e1fbb2716a491273c648a5858d
Patrick Judge <pjudge@bettergov.org>
2019-02-21 12:02:46 -0600

    edit table styles

--- frontend/src/pages/RaceDetail/FinanceFeed.jsx
+++ frontend/src/pages/RaceDetail/FinanceFeed.jsx
@@ -1,76 +1,116 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
+function formatNumber(num) {
+  if (num != null && typeof num !== undefined) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  } else {
+    return <span className='is-centered'>N/A</span>;
+  }
+}
+
 class FinanceFeed extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       sortBy: null,
       order: 'desc',
       currentSort: 'candidate'
-    }
+    };
   }
 
-  setOrder = (e) => {
-    document.querySelector('.col-sort.is-selected').classList.remove('is-selected');
+  setOrder = e => {
+    document
+      .querySelector('.col-sort.is-selected')
+      .classList.remove('is-selected');
     e.target.classList.add('is-selected');
 
     this.setState({
       sortBy: e.target.value,
-      order: e.target.dataset.order
+      order: e.target.dataset.order,
+      currentSort: e.target.dataset.name
     });
 
-    if(e.target.dataset.order === 'desc') {
-      e.target.dataset.order = 'asc'
+    if (e.target.dataset.order === 'desc') {
+      e.target.dataset.order = 'asc';
     } else {
-      e.target.dataset.order = 'desc'
+      e.target.dataset.order = 'desc';
     }
+  };
+
+  componentDidMount() {
+    this.setState({
+      sortBy: 'last_name',
+      order: 'asc',
+      currentSort: 'candidate'
+    });
   }
 
   render() {
-    const sorted = _.orderBy(this.props.candidates, `fields.${this.state.sortBy}`, this.state.order);
+    const sorted = _.orderBy(
+      this.props.candidates,
+      d => d.fields[this.state.sortBy] || '',
+      this.state.order
+    );
 
     return (
-      <div id="the-finances">
-        <div className="columns is-gapless table-row is-mobile">
-          <div className="column has-text-centered is-4">
-            <button
-              className="button is-rounded col-sort is-selected"
-              data-order="desc"
-              data-name="candidate"
-              value="first_name"
-              onClick={this.setOrder}>Candidate</button>
-          </div>
-          <div className="column has-text-centered is-4">
-            <button
-              className="button is-rounded col-sort"
-              data-order="desc"
-              data-name="on-hand"
-              value="ri_cash_on_hand"
-              onClick={this.setOrder}>Cash on hand</button>
-          </div>
-          <div className="column has-text-centered is-4">
-            <button
-              className="button is-rounded col-sort"
-              data-order="desc"
-              data-name="total"
-              value="ri_funds_raised_this_cycle"
-              onClick={this.setOrder}>Total raised</button>
-          </div>
-        </div>
-        <div>
-          {
-            sorted.map(data => (
-              <div className="columns is-gapless is-mobile table-row">
-                <div className="column has-text-centered is-4 is-lightblue-text">{data.fields.full_name}</div>
-                <div className="column has-text-centered is-4 is-white-text">${data.fields.ri_cash_on_hand}</div>
-                <div className="column has-text-centered is-4 is-white-text">${data.fields.ri_funds_raised_this_cycle}</div>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-    )
+      <section id='the-finances'>
+        <table className='table is-fullwidth'>
+          <thead>
+            <tr>
+              <th>
+                <button
+                  className='button is-text col-sort is-selected'
+                  data-order='desc'
+                  data-name='candidate'
+                  value='last_name'
+                  onClick={this.setOrder}
+                >
+                  Candidate
+                </button>
+              </th>
+              <th className='amt'>
+                <button
+                  className='button is-text col-sort'
+                  data-order='desc'
+                  data-name='on-hand'
+                  value='ri_cash_on_hand'
+                  onClick={this.setOrder}
+                >
+                  Cash on hand
+                </button>
+              </th>
+              <th className='amt'>
+                <button
+                  className='button is-text col-sort'
+                  data-order='desc'
+                  data-name='total'
+                  value='ri_funds_raised_this_cycle'
+                  onClick={this.setOrder}
+                >
+                  Total raised
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody data-current-sort={this.state.currentSort}>
+            {sorted.map(data => (
+              <tr>
+                <td data-name='candidate'>{data.fields.full_name}</td>
+                <td data-name='on-hand' className='amt'>
+                  <span className='is-pulled-left'>{'$'}</span>
+                  {formatNumber(data.fields.ri_cash_on_hand)}
+                </td>
+                <td data-name='total' className='amt'>
+                  <span className='is-pulled-left'>{'$'}</span>
+                  {formatNumber(data.fields.ri_funds_raised_this_cycle)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    );
   }
 }
 
