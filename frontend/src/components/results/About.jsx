340f1878669e56b37697e94c17f4080051a702b6
Patrick Judge <pjudge@bettergov.org>
2019-02-25 15:22:02 -0600

    edit top results language

--- frontend/src/components/results/About.jsx
+++ frontend/src/components/results/About.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const About = () => (
   <div className='notification is-warning'>
-    Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
-    dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
-    porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida
-    purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
-    consectetur adipiscing elit
+    <p className='has-text-dark'>
+      Source: Chicago Board of Elections. Results are unofficial until certified
+      on March 13.
+    </p>
+    <p className='has-text-dark'>
+      To win, candidates must receive more than 50 percent of the vote. If no
+      candidate reaches that threshold, the top two candidates will advance to a
+      run off on April 2.
+    </p>
   </div>
 );
 
