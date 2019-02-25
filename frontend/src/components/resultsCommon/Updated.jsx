625936f414f2256bf4165676274ffca596ec5641
Patrick Judge <pjudge@bettergov.org>
2019-02-25 10:35:10 -0600

    demo loading data from context

--- frontend/src/components/resultsCommon/Updated.jsx
+++ frontend/src/components/resultsCommon/Updated.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import moment from 'moment-mini';
 import * as resultsJson from './results.tmp.json';
+import * as resultsJson2 from './results.tmp.2.json';
+
+const DataContext = React.createContext(resultsJson2.default);
 
 class Updated extends Component {
+  static contextType = DataContext;
+
   render() {
-    let results = resultsJson.default;
-    return <p>Last updated: {moment(results.datetime).format('lll')}</p>;
+    let results = this.context;
+
+    return (
+      <>
+        <p>Last updated: {moment(results.datetime).format('lll')}</p>
+      </>
+    );
   }
 }
 
-export default Updated;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <DataContext.Provider value={resultsJson.default}>
+          <Updated />
+        </DataContext.Provider>
+        <Updated />
+      </>
+    );
+  }
+}
+
+export default App;
