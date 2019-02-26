5fbade55ca80f7c2af82927d5d93189cee8c5705
Patrick Judge <pjudge@bettergov.org>
2019-02-26 15:53:15 -0600

    add pct calc and sort to scraper

--- apps/scrape/scraper.py
+++ apps/scrape/scraper.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from apps.scrape.utils import lookup_json_path, get_data,\
     get_race_code, get_cand_code, get_race_name, get_cand_name,\
     get_cand_vote_total, get_race_eligible_precincts,\
-    get_race_completed_precincts
+    get_race_completed_precincts, set_cand_vote_total
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -29,24 +29,15 @@ def scrape_results():
     # pass exception to logger
     try:
         data = get_data()
-    except Exception(e):
+        # data[0] = set_cand_vote_total(data[0], 10)
+        # data[2] = set_cand_vote_total(data[2], 20)
+        results = transform_results(data)
+        write_results(results, results_output_path)
+        upload_results(results_output_path)
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
         logger.error(e)
-
-    results = transform_results(data)
-
-    # write out
-    results_json_file = open(results_output_path, 'w')
-    json.dump(results, results_json_file)
-    results_json_file.close()
-
-    # upload to s3
-    s3 = boto3.resource('s3')
-    results_json_wb = open(results_output_path, 'rb')
-    logger.info('uploading results')
-    s3.Bucket(name=s3_bucket_name).put_object(
-        Key='results.json',
-        Metadata={'Content-Type': 'text/json'},
-        Body=results_json_wb)
 
 
 def transform_results(data):
@@ -54,17 +45,19 @@ def transform_results(data):
 
     # collect results
     results = {
-        'contest_headers': ["name", "prs_rpt", "prs_tot"],
-        'cand_headers': ["name", "vote_cnt"],
+        'contest_headers': ["name", "prs_rpt", "prs_tot", "vote_tot"],
+        'cand_headers': ["name", "vote_cnt", "vote_pct"],
         'datetime': now.strftime(timestamp_format),
         'timestamp': int(now.replace(microsecond=0).timestamp()),
         'contests': {},
-        "cand_classes": ["", "amt append-bar"],
+        "cand_classes": ["", "amt", "amt append-bar"],
     }
+    # TODO The current code is super dependent on ^this^ not changing. Brittle!
 
     # loop through scraped data
     # and append to results
     races = []  # keeps track of which results are in
+
     for row in data:
         # parse data from page,
         # row by row
@@ -97,7 +90,56 @@ def transform_results(data):
         # adds candidate-level data
         # to results['contests'][race]['candidates']
         results['contests'][race_code]['cands'].append(
-            [cv_cand_name, cand_vote_total]
-        )
+            [cv_cand_name, cand_vote_total])
+
+    # calc percentages and add to results
+
+    if 'vote_pct' in results['cand_headers']:
+        for key in results['contests']:
+            cand_items = results['contests'][key]['cands']
+            cand_votes = [x[1] for x in cand_items]
+            total_votes = sum(cand_votes)
+
+            results['contests'][key]['meta'] += [total_votes]
+
+            for cand_item in cand_items:
+                votes = cand_item[1]
+                cand_item += [_calc_percent(votes, total_votes)]
+
+            # sort by vote total
+            cand_items = cand_items.sort(key=_sort_by_vote, reverse=True)
+    else:
+        pass
 
     return results
+
+
+def _sort_by_vote(val):
+    return val[1]
+
+
+def _calc_percent(val, total):
+    '''
+    Helper function to get pct of total votes as a string.
+    '''
+    if (total > 0):
+        return "%.1f%%" % (val/total * 100)
+    else:
+        return "N/A"
+
+
+def write_results(results, path):
+    # write out
+    with open(path, 'w') as jsonfile:
+        json.dump(results, jsonfile)
+
+
+def upload_results(path):
+    # upload to s3
+    s3 = boto3.resource('s3')
+    results_json_wb = open(path, 'rb')
+    logger.info('uploading results')
+    s3.Bucket(name=s3_bucket_name).put_object(
+        Key='results.json',
+        Metadata={'Content-Type': 'text/json'},
+        Body=results_json_wb)
--- apps/scrape/utils.py
+++ apps/scrape/utils.py
@@ -5,7 +5,8 @@ see data:
 and layout:
     https://chicagoelections.com/results/ap/text_layout.txt
 """
-import requests, json
+import requests
+import json
 from apps.candidates.models import Candidate
 from apps.races.models import Race
 from chivote.settings.base import BASE_DIR
@@ -22,10 +23,10 @@ race_name_range_start, race_name_range_end = 32, 87
 cand_name_range_start, cand_name_range_end = 88, 126
 cand_code_range_start, cand_code_range_end = 0, 7
 
-cand_vote_range_start, cand_vote_range_end = 11,18
+cand_vote_range_start, cand_vote_range_end = 11, 18
 
-race_elig_precinct_range_start, race_elig_precinct_range_end = 7,11
-race_comp_precinct_range_start, race_comp_precinct_range_end = 18,22
+race_elig_precinct_range_start, race_elig_precinct_range_end = 7, 11
+race_comp_precinct_range_start, race_comp_precinct_range_end = 18, 22
 
 ### END CONFIG ###
 
@@ -59,6 +60,13 @@ def get_cand_vote_total(line):
     return int(line[cand_vote_range_start:cand_vote_range_end])
 
 
+def set_cand_vote_total(line, amt):
+    new_amt = str(amt).zfill(cand_vote_range_end - cand_vote_range_start)
+    new_line = line[:cand_vote_range_start] + \
+        new_amt + line[cand_vote_range_end:]
+    return new_line
+
+
 def get_race_eligible_precincts(line):
     # the total number of precincts in the ward/city
     return int(line[race_elig_precinct_range_start:race_elig_precinct_range_end])
@@ -81,33 +89,35 @@ def make_lookup_json(data=get_data()):
         },
     }
     """
-    lookup = {'races':dict(),'candidates':dict()}
+    lookup = {'races': dict(), 'candidates': dict()}
     for line in get_data():
         # add candidates
         cboe_cand_results_code = get_cand_code(line)
         cboe_cand_results_name = get_cand_name(line)
         try:
-            candidate = Candidate.objects.get(cboe_results_id=cboe_cand_results_code)
+            candidate = Candidate.objects.get(
+                cboe_results_id=cboe_cand_results_code)
         except Exception as e:
             print(e)
-            import ipdb; ipdb.set_trace()
+            import ipdb
+            ipdb.set_trace()
         lookup['candidates'][cboe_cand_results_code] = {
-                'chi_vote_name':candidate.full_name,
-                'cboe_results_name':cboe_cand_results_name,
-                }
+            'chi_vote_name': candidate.full_name,
+            'cboe_results_name': cboe_cand_results_name,
+        }
         # add races, if not added already
         cboe_race_results_code = get_race_code(line)
         if cboe_race_results_code in lookup['races']:
             continue
         cboe_race_results_name = get_race_name(line)
         race = Race.objects.get(cboe_results_id=cboe_race_results_code)
- 
+
         lookup['races'][cboe_race_results_code] = {
-                'chi_vote_name':race.results_slug,
-                'cboe_results_name':cboe_race_results_name,
-                }
-    
+            'chi_vote_name': race.results_slug,
+            'cboe_results_name': cboe_race_results_name,
+        }
+
     # write out
-    lookup_file = open(lookup_json_path,'w',encoding='utf-8')
-    json.dump(lookup,lookup_file,ensure_ascii=False)
+    lookup_file = open(lookup_json_path, 'w', encoding='utf-8')
+    json.dump(lookup, lookup_file, ensure_ascii=False)
     lookup_file.close()
